db1a7747e696832f408aeffef3b45ce5dd709af3
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2021-05-27 16:18:29 +0100

    Allowing the script to click the `Next` button as well

--- Scripts/Microsoft/device-auth-injector.js
+++ Scripts/Microsoft/device-auth-injector.js
@@ -2,7 +2,7 @@
 // @name         Inject Device Auth OTP
 // @description  Injects the query parameter value of otp into the field with the ID of otc
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @author       Will Dewhurst
 // @include      https://login.microsoftonline.com/common/oauth2/deviceauth?otp=*
 // @icon         https://www.google.com/s2/favicons?domain=microsoftonline.com
@@ -13,5 +13,6 @@
     'use strict';
     setTimeout(function(){
         document.getElementById('otc').value = window.location.search.split('=')[1];
+        document.getElementById('idSIButton9').click();
     }, 500);
 })();
