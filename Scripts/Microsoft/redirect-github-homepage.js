cbc8ae7ad77869681be0418c311f344d85f7136e
Will Dewhurst <will.dewhurst@ansgroup.co.uk>
2021-05-27 16:50:05 +0100

    Adding redirect-github-homepage.js

---
+++ Scripts/Microsoft/redirect-github-homepage.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Redirect GitHub
+// @description  Redirect GitHub homepage to user overview
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Will Dewhurst
+// @match        https://github.com
+// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @grant        none
+// ==/UserScript==
+
+// https://github.com will redirect you to your account overview
+// https://github.com/# will take you to the github homepage
+(function() {
+    'use strict';
+    const username = 'dewhurstwill'
+    if (!(window.location.href.includes('/#'))) {
+      window.location.href = `https://github.com/${username || '#'}`;
+    }
+})();
+
